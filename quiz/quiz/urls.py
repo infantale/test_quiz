8654a8aee30dade5cf4a725ed0ad91636a70a5c8
infantale <71599070+infantale@users.noreply.github.com>
2021-11-11 21:42:10 +0300

    Add files via upload

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from core.views import QuizViewSet, QuestionViewSet
+
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Quiz API')
 
 router = SimpleRouter()
 
@@ -26,6 +30,7 @@ router.register(r'quiz', QuizViewSet)
 router.register(r'question', QuestionViewSet)
 
 urlpatterns = [
+    url(r'^$', schema_view),
     path('admin/', admin.site.urls),
     path('user/', include('core.urls')),
 ]
